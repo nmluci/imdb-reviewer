c7d734c2117bc947183ede250ccc79bf4773a7e0
Lynne Fuyuna <yryuu@outlook.com>
2023-06-17 21:43:29 +0800

    feat: add basic inference script and model

--- ml-engine/app.py
+++ ml-engine/app.py
@@ -1,27 +1,43 @@
-import torch
-from transformers import AutoTokenizer
-from transformers import BertForSequenceClassification
+import tensorflow as tf
+from keras.preprocessing.text import tokenizer_from_json
+from keras.utils import pad_sequences
 import numpy as np
+import json
 
-sentichan = BertForSequenceClassification.from_pretrained('bert-base-uncased',
-                                                          num_labels=2,
-                                                          output_attentions=False,
-                                                          output_hidden_states=False)
+sentences = [
+   "I can't believe you betrayed my trust like that!",  # Anger
+   "Every creak in the house fills me with terror, imagining the worst.",  # Fear
+   "The sight of her smile instantly brightens my day and fills my heart with happiness.",  # Joy
+   "Being in your arms makes me feel complete, and I cherish every moment with you.",  # Love
+   "The weight of loss sits heavy on my heart, and tears flow freely down my cheeks.",  # Sadness
+   "As I opened the gift, a surge of excitement rushed through me. I never expected such a thoughtful gesture!",  # Surprise
+   "im updating my blog because i feel shitty",
+]
 
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', lower=True)
+emotion_map =  {
+   0: "anger",
+   1: "fear",
+   2: "joy",
+   3: "love",
+   4: "sadness",
+   5: "surpise"
+}
 
-sentichan.load_state_dict(torch.load("sentichan_v2_state.pt", map_location=torch.device('cpu')))
-sentichan.eval()
 
-for param in sentichan.state_dict():
-    print(param, "\t", sentichan.state_dict()[param].size())
+model = tf.keras.models.load_model("model/sentichan.h5")
 
-# with torch.no_grad():
-#    input_text = "this review is perfectly majestic"
-#    encoded = tokenizer(input_text, truncation=True)
+with open("model/tokenizer.json") as f:
+   tokenizer = tokenizer_from_json(json.load(f))
 
-#    text = torch.tensor(encoded["input_ids"]).unsqueeze(0)
-#    attention_mask = torch.tensor(encoded["attention_mask"]).unsqueeze(0)
+sen_seq = tokenizer.texts_to_sequences(list(v.lower() for v in sentences))
+sen_pad = pad_sequences(sen_seq, padding='post', maxlen=40, truncating='post')
 
-#    output = sentichan(text, attention_mask)
-
+pred = model.predict(sen_pad)
+for sen, s in zip(sentences, pred):
+   print(">> ", sen)
+   emotion_idx = np.argmax(s)
+   emotion_map[np.argmax(s)]
+   
+   for idx, v in enumerate(s):
+      print("{}: {:.5f}".format(emotion_map[idx], v))
+   print()
